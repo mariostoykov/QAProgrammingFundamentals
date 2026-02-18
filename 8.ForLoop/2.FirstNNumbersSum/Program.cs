8541c6dd765c8bd7edd2ac7f9e13474069b27c56
mariostoykov <mario.stoykov12@gmail.com>
2026-02-18 20:57:21 +0200

    8.ForLoop

---
+++ 8.ForLoop/2.FirstNNumbersSum/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+string expression = "";
+for (int i = 1; i <= n; i++)
+{
+    sum += i;
+    if (i == n)
+    {
+     expression += $"{i}=";
+    }
+    else
+    {
+     expression += $"{i}+";
+    }
+}
+Console.WriteLine($"{expression}{sum}");
+
---
+++ 8.ForLoop/7.LatinLetters/Program.cs
@@ -0,0 +1,10 @@
+﻿char start = char.Parse(Console.ReadLine());
+char end = char.Parse(Console.ReadLine());
+
+for (int i = start; i <= end; i++)
+{
+    if (i >= 'a' &&  i <= 'z' ||  i >= 'A' &&  i <= 'Z')
+    {
+        Console.Write($"{(char)i} ");
+    }
+}
