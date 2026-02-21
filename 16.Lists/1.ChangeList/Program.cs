6a9d4cca587adcd9b8982481b2988e97a772b33d
mariostoykov <mario.stoykov12@gmail.com>
2026-02-21 12:38:33 +0200

    16.Lists

---
+++ 16.Lists/1.ChangeList/Program.cs
@@ -0,0 +1,33 @@
+﻿
+List<int> numbers = Console.ReadLine()
+                            .Split(" ")
+                            .Select(int.Parse)
+                            .ToList();
+
+string[] cmdArg = Console.ReadLine().Split();
+
+while (cmdArg[0] != "end")
+{
+    string command = cmdArg[0];
+    if (command == "Delete")
+    {
+        int elementToDelete = int.Parse(cmdArg[1]);
+
+        for (int i = 0; i < numbers.Count; i++)
+        numbers.Remove(elementToDelete);
+    }
+
+    else if (command == "Insert")
+    {
+        int elementToInsert = int.Parse(cmdArg[1]);
+        int index = int.Parse(cmdArg[2]);
+
+        numbers.Insert(index, elementToInsert);
+    }
+
+    cmdArg = Console.ReadLine().Split();
+}
+
+Console.WriteLine(string.Join(" ", numbers));
+
+
---
+++ 16.Lists/2.ListOfProducts/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+List<string> products = new List<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string currentProduct = Console.ReadLine();
+    products.Add(currentProduct);
+}
+
+products.Sort();
+
+int counter = 1;
+
+foreach (string product in products)
+{
+    Console.WriteLine($"{counter}.{product}");
+    counter++;
+}
