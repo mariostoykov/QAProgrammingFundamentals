7659a0e9bc71f998f82a8309ce6b15927aa93a9e
mariostoykov <mario.stoykov12@gmail.com>
2026-02-21 12:27:28 +0200

    14.Arrays

---
+++ 14.Arrays/2.SumAnArray/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int sum = 0;
+
+foreach (int number in numbers)
+{
+    sum += number;
+}
+
+Console.WriteLine(sum);
---
+++ 14.Arrays/3.ReverseAnArray/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int countNumbers = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[countNumbers];
+
+for (int number = 1; number <= countNumbers; number++)
+{
+    int value = int.Parse(Console.ReadLine());
+    numbers[number - 1] = value;
+}
+
+for (int position = numbers.Length - 1; position >= 0; position--)
+{
+    Console.Write(numbers[position] + " ");
+}
